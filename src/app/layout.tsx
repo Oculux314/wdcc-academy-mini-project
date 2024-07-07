39d1d34e2e84c5f6c470d2ce83e5c46d08a732ed
Oculux <nate.williamson.nz@gmail.com>
2024-07-07 23:17:14 +1200

    :sparkles: create navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
+import NavBar from "@/components/NavBar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import pfp from "@/assets/pfp.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nate's Blog Site",
   description: "For the 2024 WDCC Academy Programme",
+  icons: [
+    {
+      rel: "icon",
+      url: "/pfp.png",
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -16,7 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,26 @@
+import pfp from "@/../public/pfp.png";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="py-5 px-20 bg-slate-300">
+      <ul className="flex items-center gap-10">
+        <li className="mr-auto">
+          <Link href="/">
+            <Image src={pfp} alt="Oculux" className="rounded-full h-8 w-8" />
+          </Link>
+        </li>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/blogs">Blogs</Link>
+        </li>
+        <li>
+          <Link href="/instructions">Instructions</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
